fb5f65004d6b372573bb5cfc9b4379d4ad987848
zhanghongy <865383353@qq.com>
2019-04-23 16:13:12 +0800

    Initial commit

---
+++ itoken-admin/src/main/java/com/zhang/itoken/admin/ItokenAdminApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.itoken.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenAdminApplication.class, args);
+    }
+
+}
---
+++ itoken-commons/src/main/java/com/zhang/itoken/commons/ItokenCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.itoken.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenCommonsApplication.class, args);
+    }
+
+}
