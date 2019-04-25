be84a04aa15b56fc3e2bf41cf796fb0dc001497d
zhanghongy <865383353@qq.com>
2019-04-25 15:30:07 +0800

    config-8888

--- itoken-config-8888/src/main/java/com/zhang/itoken/config/ItokenConfigApplication.java
+++ itoken-config-8888/src/main/java/com/zhang/itoken/config/ItokenConfigApplication.java
@@ -2,7 +2,16 @@ package com.zhang.itoken.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+/**
+ * @Author 张宏运
+ * @Date  2019-04-25 14:19
+ * 配置中心
+ * port：8888
+ */
+@EnableEurekaClient
+@EnableConfigServer
 @SpringBootApplication
 public class ItokenConfigApplication {
 
