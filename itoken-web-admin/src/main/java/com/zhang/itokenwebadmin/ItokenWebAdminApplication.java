7a0d2866f9d4833de2624f9f7d395208b0eab8b0
zhanghongy <865383353@qq.com>
2019-05-13 16:07:23 +0800

    modify

---
+++ itoken-web-admin/src/main/java/com/zhang/itokenwebadmin/ItokenWebAdminApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.itokenwebadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenWebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenWebAdminApplication.class, args);
+    }
+
+}
