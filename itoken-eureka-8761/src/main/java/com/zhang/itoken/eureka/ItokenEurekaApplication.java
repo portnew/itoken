23d13d4077a25eb6293697187a5d45aaa10b79f1
zhanghongy <865383353@qq.com>
2019-05-13 15:21:31 +0800

    modify

--- itoken-eureka-8761/src/main/java/com/zhang/itoken/eureka/ItokenEurekaApplication.java
+++ itoken-eureka-8761/src/main/java/com/zhang/itoken/eureka/ItokenEurekaApplication.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class ItokenEurekaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ItokenEurekaApplication.class, args);
     }
 
